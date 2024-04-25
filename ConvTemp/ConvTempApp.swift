adb7c1a97183211150f7fc77bbef390c70f570c5
Neeta Buhecha <neeta.b@gmail.com>
2024-04-25 15:54:41 +0100

    Initial Commit

---
+++ ConvTemp/ConvTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvTempApp.swift
+//  ConvTemp
+//
+//  Created by Neeta Buhecha on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConvTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
